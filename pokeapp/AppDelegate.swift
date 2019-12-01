b6933d7732cb6998ba8d42b0cfd5b5bbe0584e57
Mateus Cerqueira <mateus.mcg@gmail.com>
2019-12-01 11:22:44 -0300

    Refactor on the storyboards

--- pokeapp/AppDelegate.swift
+++ pokeapp/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UITabBar.appearance().barTintColor = .black
-        UITabBar.appearance().tintColor = .red
+//        UITabBar.appearance().barTintColor = .black
+//        UITabBar.appearance().tintColor = .red
         return true
     }
 
