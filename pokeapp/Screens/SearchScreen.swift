5aacc1b2de20000126c554ec601c886c229086bb
Mateus Cerqueira <mateus.mcg@gmail.com>
2019-12-12 21:42:12 -0300

    Added auto layout to search storyboard

--- pokeapp/Screens/SearchScreen.swift
+++ pokeapp/Screens/SearchScreen.swift
@@ -13,40 +13,33 @@ class SearchScreen: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    @IBOutlet weak var pokemonContainer: UIView!
-    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonSprite: UIImageView!
     @IBOutlet weak var pokemonName: UILabel!
     @IBOutlet weak var pokemonTypes: UILabel!
-    
-    @IBOutlet weak var pokemonAtkValue: UILabel!
-    @IBOutlet weak var pokemonDefValue: UILabel!
-    @IBOutlet weak var pokemonHpValue: UILabel!
-    @IBOutlet weak var pokemonSpecialAtkValue: UILabel!
-    @IBOutlet weak var pokemonSpecialDefValue: UILabel!
-    @IBOutlet weak var pokemonSpeedValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        navigationItem.title = "PokéApp"
     }
     
     func setupPokemonInPage(pokemon: Pokemon) -> Void {
-        self.pokemonContainer.isHidden = false
-        self.pokemonImage.downloaded(from: pokemon.sprites.front_default!)
+//        self.pokemonContainer.isHidden = false
+        self.pokemonSprite.downloaded(from: pokemon.sprites.front_default!)
         self.pokemonName.text = String(pokemon.id) + " - " + pokemon.name.capitalized
         self.pokemonTypes.text = pokemon.types.map { $0.type.name.capitalized }.joined(separator: " - ")
-        
-        self.pokemonAtkValue.text = String(pokemon.stats.filter { $0.stat.name == "attack"}[0].base_stat)
-        
-        self.pokemonDefValue.text = String(pokemon.stats.filter { $0.stat.name == "defense"}[0].base_stat)
-        
-        self.pokemonHpValue.text = String(pokemon.stats.filter { $0.stat.name == "hp"}[0].base_stat)
-        
-        self.pokemonSpecialAtkValue.text = String(pokemon.stats.filter { $0.stat.name == "special-attack"}[0].base_stat)
-        
-        self.pokemonSpecialDefValue.text = String(pokemon.stats.filter { $0.stat.name == "special-defense"}[0].base_stat)
-        
-        self.pokemonSpeedValue.text = String(pokemon.stats.filter { $0.stat.name == "speed"}[0].base_stat)
+//
+//        self.pokemonAtkValue.text = String(pokemon.stats.filter { $0.stat.name == "attack"}[0].base_stat)
+//
+//        self.pokemonDefValue.text = String(pokemon.stats.filter { $0.stat.name == "defense"}[0].base_stat)
+//
+//        self.pokemonHpValue.text = String(pokemon.stats.filter { $0.stat.name == "hp"}[0].base_stat)
+//
+//        self.pokemonSpecialAtkValue.text = String(pokemon.stats.filter { $0.stat.name == "special-attack"}[0].base_stat)
+//
+//        self.pokemonSpecialDefValue.text = String(pokemon.stats.filter { $0.stat.name == "special-defense"}[0].base_stat)
+//
+//        self.pokemonSpeedValue.text = String(pokemon.stats.filter { $0.stat.name == "speed"}[0].base_stat)
     }
 }
 
